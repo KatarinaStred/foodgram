4d3f62c8220ec97c6fcb3f7f0eff2951cc46fa3a
Катерина Михайлова <ketistred@yandex.ru>
2025-02-14 14:25:26 +0300

    corr import data v2

--- backend/recipes/management/commands/import_data.py
+++ backend/recipes/management/commands/import_data.py
@@ -2,7 +2,7 @@ import csv
 import os
 from datetime import datetime
 
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand  # , CommandError
 from django.db import IntegrityError
 from foodgram.settings import BASE_DIR
 from recipes.models import Ingredient, Tag
@@ -17,39 +17,35 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         start_time = datetime.now()
+        with open(
+            os.path.join(FILE_PATH, 'ingredients.csv')
+        ) as file_csv:
+            data = csv.reader(file_csv)
+            for row in data:
+                try:
+                    obj, created = Ingredient.objects.get_or_create(
+                        name=row['name'],
+                        measurement_unit=row['measurement_unit'],)
+                    if not created:
+                        print(f'Ингредиент {obj} уже есть в базе.')
+                except IntegrityError as err:
+                    print(f'Ошибка в строке {row}: {err} по ингредиентам.')
+            print('Файл ingredients.csv успешно импортирован')
 
-        try:
-            with open(
-                os.path.join(FILE_PATH, 'ingredients.csv')
-            ) as file_csv:
-                data = csv.DictReader(file_csv, delimiter=',')
-                for row in data:
-                    try:
-                        obj, created = Ingredient.objects.create(
-                            name=row['name'],
-                            measurement_unit=row['measurement_unit'])
-                        if not created:
-                            print(f'Ингредиент {obj} уже есть в базе.')
-                    except IntegrityError as err:
-                        print(f'Ошибка в строке {row}: {err} по ингредиентам.')
-                print('Файл ingredients.csv успешно импортирован')
-
-            with open(
-                os.path.join(FILE_PATH, 'tags.csv')
-            ) as file_csv:
-                data = csv.DictReader(file_csv, delimiter=',')
-                for row in data:
-                    try:
-                        obj, created = Tag.objects.create(
-                            name=row['name'],
-                            slug=row['slug'])
-                        if not created:
-                            print(f'Тег {obj} уже есть в базе.')
-                    except IntegrityError as err:
-                        print(f'Ошибка в строке {row}: {err} по тегам.')
-                print('Файл tags.csv успешно импортирован')
-        except Exception as error:
-            raise CommandError(f'Произошла ошибка: {error}')
+        with open(
+            os.path.join(FILE_PATH, 'tags.csv')
+        ) as file_csv:
+            data = csv.reader(file_csv)
+            for row in data:
+                try:
+                    obj, created = Tag.objects.get_or_create(
+                        name=row['name'],
+                        slug=row['slug'])
+                    if not created:
+                        print(f'Тег {obj} уже есть в базе.')
+                except IntegrityError as err:
+                    print(f'Ошибка в строке {row}: {err} по тегам.')
+            print('Файл tags.csv успешно импортирован')
 
         end_time = datetime.now()
         duration = end_time - start_time
